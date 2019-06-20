38e86ac9b1ee6f00666f1b988279e5456b078c7c
Anthony Diké <anthonydike@nyu.edu>
2019-06-20 13:21:50 -0400

    Added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import InputBar from "./components/InputBar";
+import Headline from "./components/Headline";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Search Bar</h1>
+      <Headline />
       <InputBar />
     </div>
   );
