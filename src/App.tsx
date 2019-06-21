a6de3e1b04326ac3e9f147445db1d20a28f8f89d
Anthony Diké <anthonydike@nyu.edu>
2019-06-21 12:41:50 -0400

    Centered both ways

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Headline from "./components/Headline";
 
 const appStyle = {
   textAlign: "center" as "center",
-  margin: "auto"
+  margin: "auto",
+  display: "block",
+  padding: "275px 0"
 };
 
 const App: React.FC = () => {
